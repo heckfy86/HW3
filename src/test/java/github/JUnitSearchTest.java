d64105aa6e905a3d455f4718a920c76657056301
gmail <gmail08@yandex.ru>
2023-01-07 10:58:28 +0100

    11

--- src/test/java/github/JUnitSearchTest.java
+++ src/test/java/github/JUnitSearchTest.java
@@ -2,15 +2,18 @@ package github;
 import com.codeborne.selenide.Selenide;
 import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class  JUnitSearchTest  {
     @Test
-    void FindJUnit5Example() {
+    void findJUnit5Example() {
 
         Selenide.open("https://github.com/selenide/selenide");
         $("[id=wiki-tab]").click();
-        $(".markdown-body").$$("ul li").find(text("Soft assertions")).$("a").click();
-        $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
+        $("#wiki-pages-filter").setValue("Softassertions").pressEnter();
+        $("#wiki-pages-box").shouldHave(text("SoftAssertions"));
+        $(byText("SoftAssertions")).click();
+        $("div.markdown-body").shouldHave(text("Using JUnit5 extend test class"));
     }
 }
